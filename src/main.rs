3429c17a33ac6059721cdde2d722d2b8bbae668a
Giovanni Rella <rellag@nida.nih.gov>
2024-05-01 16:20:25 -0400

    enabling replies in threads

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 use lambda_runtime::{service_fn, LambdaEvent};
+use regex::Regex;
 use serde_json::{json, Value};
 use std::collections::HashMap;
-use regex::Regex;
 mod spotify;
 mod youtube;
 
 #[tokio::main]
 async fn main() -> Result<(), lambda_runtime::Error> {
-        lambda_runtime::run(service_fn(func)).await?;
-        Ok(())
+    lambda_runtime::run(service_fn(func)).await?;
+    Ok(())
 }
 
 async fn func(event: LambdaEvent<Value>) -> Result<Value, lambda_runtime::Error> {
-        get_slack_api_key();
-        let (event, _context) = event.into_parts();
-        println!("event: {}\n", event);
-        //let body = event.as_str().unwrap();
-        let body = event["body"].as_str().unwrap();
-        let body: HashMap<String, Value> = serde_json::from_str(body).unwrap();
-        if body.contains_key("challenge") && body["challenge"] != json!(null) {
-            return Ok(json!({ "challenge": body["challenge"] }));
-        }
-        respond_to_slack_event(&body).await?;
-        return Ok(json!({ "message": "Success!" }));
+    get_slack_api_key();
+    let (event, _context) = event.into_parts();
+    println!("event: {}\n", event);
+    //let body = event.as_str().unwrap();
+    let body = event["body"].as_str().unwrap();
+    let body: HashMap<String, Value> = serde_json::from_str(body).unwrap();
+    if body.contains_key("challenge") && body["challenge"] != json!(null) {
+        return Ok(json!({ "challenge": body["challenge"] }));
+    }
+    respond_to_slack_event(&body).await?;
+    return Ok(json!({ "message": "Success!" }));
 }
 
 fn get_slack_api_key() -> String {
@@ -36,7 +36,7 @@ fn get_slack_api_key() -> String {
         }
     }
 }
-async fn respond_to_slack_event(body: &HashMap<String, Value>)  -> Result<(), reqwest::Error> {
+async fn respond_to_slack_event(body: &HashMap<String, Value>) -> Result<(), reqwest::Error> {
     //println!("body: {:?}\n", body);
     let event = &body["event"];
     //println!("event: {:?}\n", event);
@@ -45,7 +45,7 @@ async fn respond_to_slack_event(body: &HashMap<String, Value>)  -> Result<(), re
     let user_id = &event["user"].as_str().unwrap();
     let rusty_user_id = &body["authorizations"][0]["user_id"]
         .as_str()
-        .unwrap_or_else(|| "U01UTH2J666");//rusty user id
+        .unwrap_or_else(|| "U01UTH2J666"); //rusty user id
     if event["bot_id"] == json!(null) {
         let mut message = String::new();
         let regex_video = Regex::new(r".*, play that video ").unwrap();
@@ -69,24 +69,32 @@ async fn respond_to_slack_event(body: &HashMap<String, Value>)  -> Result<(), re
             message = spotify::search_spotify(&artist, spotify::SearchType::Artist).await?;
         }
         if message != String::new() {
-            post_message(channel, message.as_str()).await?
-        } 
+            post_message(channel, message.as_str(), event["thread_ts"].as_str()).await?
+        }
     } else {
         println!("Bot message! bot_id: {}\n", event["bot_id"]);
     }
     Ok(())
 }
 
-async fn post_message(channel: &str, message: &str) -> Result<(), reqwest::Error> {
+async fn post_message(
+    channel: &str,
+    message: &str,
+    thread_ts: Option<&str>,
+) -> Result<(), reqwest::Error> {
     let token = get_slack_api_key();
     let mut body = HashMap::new();
     body.insert("channel", channel);
     body.insert("text", message);
+    if let Some(thread_ts) = thread_ts {
+        body.insert("thread_ts", thread_ts);
+    }
     let client = reqwest::Client::new();
     client
         .post("https://slack.com/api/chat.postMessage")
         .json(&body)
         .header("Authorization", format!("Bearer {}", token))
-        .send().await?;
+        .send()
+        .await?;
     Ok(())
 }
